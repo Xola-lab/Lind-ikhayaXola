3fa5bae2189db720815fa3091db61b08ac88d65f
Lind'ikhaya Xola <73102194+Xola-lab@users.noreply.github.com>
2024-06-11 03:00:05 +0200

    Add files via upload

---
+++ submit_form.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $POST["message"];
+
+    $to = "xolal5783@gmail.com";
+    $subject = "New Message From Your Website";
+    $body = "Name: $name\nEmail: $email\nMessage:n$message"
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!"
+    } else {
+        echo "Oops! Something went wrong."
+    }
+}
+?>
